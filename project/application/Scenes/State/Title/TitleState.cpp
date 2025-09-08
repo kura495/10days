874452174d3d36c024df6f0790c056554106904a
雄大 渡辺 <veliano393386@gmail.com>
2025-09-08 10:21:31 +0900

    遷移ok 画像等のリソース要修正

--- project/application/Scenes/State/Title/TitleState.cpp
+++ project/application/Scenes/State/Title/TitleState.cpp
@@ -9,7 +9,7 @@ void TitleState::Init()
 
 	followCamera = std::make_unique<FollowCamera>();
 	followCamera->Initialize();
-	followCamera->GetParameter().translation_ = {0.0f,0.0f,-10.0f};
+	followCamera->GetParameter().translation_ = { 0.0f,0.0f,-10.0f };
 
 	titleSprite = std::make_unique<Sprite>();
 	titleSprite->Init({ 0.0f,0.0f }, { 0.0f,720.0f }, { 1280.0f,0.0f }, { 1280.0f,720.0f });
@@ -25,11 +25,26 @@ void TitleState::Update()
 {
 	Renderer::SetViewProj(followCamera->GetParameter());
 
+	if (Input::GetPadPrecede(XINPUT_GAMEPAD_A, 10)) {
+		//ifの条件は後で変える
+		if (StateNo != GameStateNo::CLEAR) {
+			StateNo = GameStateNo::CLEAR;
+		}
+	}
+
+	if (Input::GetPadPrecede(XINPUT_GAMEPAD_B, 10)) {
+		//ifの条件は後で変える
+		if (StateNo != GameStateNo::GAMEOVER) {
+			StateNo = GameStateNo::GAMEOVER;
+		}
+	}
+
+
 }
 
 void TitleState::Draw()
 {
 
 	titleSprite->RendererDraw(title);
-	texture->RendererDraw(title);
+	//texture->RendererDraw(title);
 }
